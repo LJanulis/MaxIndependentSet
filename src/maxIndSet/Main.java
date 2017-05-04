7d22cb4677726a52d0b612ed24f89ac685d6b6b2
LUKASJ <lukas.janulis@gmail.com>
2017-05-04 16:33:53 +0300

    Algoritmu teorija nd5

--- src/maxIndSet/Main.java
+++ src/maxIndSet/Main.java
@@ -41,8 +41,8 @@ public class Main {
                         System.out.println("Enter number of repeats:");
                         int repeats = Integer.parseInt(sc.nextLine());
 
-                        System.out.println("Testing recursive algorithm on " + repeats + " random graphs with 15 vertices and 20 - 60 edges");
-                        testAlgorithms(25, 25, 0, 40, repeats, true);
+                        System.out.println("Testing recursive algorithm on " + repeats + " random graphs with 25 vertices and 0 - 60 edges");
+                        testAlgorithms(15, 15, 0, 60, repeats, true);
                         System.out.println();
                         System.out.println("Testing recursive algorithm on " + repeats + " random graphs with 7 - 30 vertices and 20 edges");
                         testAlgorithms(7, 30, 20, 20, repeats, true);
@@ -51,8 +51,8 @@ public class Main {
                         System.out.println("Enter number of repeats:");
                         repeats = Integer.parseInt(sc.nextLine());
 
-                        System.out.println("Testing backtracking algorithm on " + repeats + " random graphs with 15 vertices and 20 - 60 edges");
-                        testAlgorithms(15, 15, 20, 60, repeats, false);
+                        System.out.println("Testing backtracking algorithm on " + repeats + " random graphs with 15 vertices and 0 - 60 edges");
+                        testAlgorithms(15, 15, 0, 60, repeats, false);
                         System.out.println();
                         System.out.println("Testing backtracking algorithm on " + repeats + " random graphs with 7 - 20 vertices and 20 edges");
                         testAlgorithms(7, 20, 20, 20, repeats, false);
